ba02915c38b664d0e22e4e431291c90b08f3a2aa
Dhanu Shrijan Boorlagadda <dhanushrijanboorlagadda@gmail.com>
2026-06-21 15:23:22 +0530

    Add decoder2x4 project

---
+++ decoder2x4.v
@@ -0,0 +1,13 @@
+module decoder2x4 (
+    input A,
+    input B,
+    output Y0,
+    output Y1,
+    output Y2,
+    output Y3
+);
+    assign Y0 = ~A & ~B;
+    assign Y1 = ~A & B;
+    assign Y2 = A & ~B;
+    assign Y3 = A & B;
+endmodule
---
+++ decoder2x4_tb.v
@@ -0,0 +1,40 @@
+module decoder2x4_tb;
+reg A;
+reg B;
+wire Y0;
+wire Y1;
+wire Y2;
+wire Y3;
+
+decoder2x4 uut (
+    .A(A),
+    .B(B),
+    .Y0(Y0),
+    .Y1(Y1),
+    .Y2(Y2),
+    .Y3(Y3)
+);
+initial begin
+    $dumpfile("decoder2x4_tb.vcd");
+    $dumpvars(0, decoder2x4_tb);
+    $display("A B | Y0 Y1 Y2 Y3");
+
+    A=0; B=0;
+    #10;
+    $display("%b %b | %b %b %b %b",A,B,Y0,Y1,Y2,Y3);
+
+    A=0; B=1;
+    #10;
+    $display("%b %b | %b %b %b %b",A,B,Y0,Y1,Y2,Y3);
+    
+    A=1; B=0;
+    #10;
+    $display("%b %b | %b %b %b %b",A,B,Y0,Y1,Y2,Y3);
+    
+    A=1; B=1;
+    #10;
+    $display("%b %b | %b %b %b %b",A,B,Y0,Y1,Y2,Y3);
+    $finish;
+end 
+endmodule
+    
